abf1ebcf5430cb4f37300434a62216ecca8bfdc6
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-07-13 19:43:40 -0600

    Nuevos atributos a UserDTO

--- src/main/java/com/demo/pdf/dto/UserDTO.java
+++ src/main/java/com/demo/pdf/dto/UserDTO.java
@@ -1,38 +1,13 @@
 package com.demo.pdf.dto;
 
-public class UserDTO
-{
-    private String firstName;
-    private String lastName;
-    private Long uid;
+import lombok.Data;
 
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public void setFirstName(final String firstName)
-    {
-        this.firstName = firstName;
-    }
-
-    public Long getUid()
-    {
-        return uid;
-    }
-
-    public void setUid(final Long uid)
-    {
-        this.uid = uid;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
-
-    public void setLastName(final String lastName)
-    {
-        this.lastName = lastName;
-    }
+@Data
+public class UserDTO {
+  private String firstName;
+  private String lastName;
+  private String logo;
+  private Long uid;
+  private String issueDate;
+  private String period;
 }
