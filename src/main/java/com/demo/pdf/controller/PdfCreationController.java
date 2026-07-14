2f1ce206bb2563721b77d53d4e4e08591b3e5815
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-07-13 19:44:32 -0600

    Refator

--- src/main/java/com/demo/pdf/controller/PdfCreationController.java
+++ src/main/java/com/demo/pdf/controller/PdfCreationController.java
@@ -1,28 +1,31 @@
 package com.demo.pdf.controller;
 
 import com.demo.pdf.facade.PdfCreationFacade;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class PdfCreationController
-{
-    @Autowired
-    PdfCreationFacade pdfCreationFacade;
+// @Controller
+@RestController
+@RequestMapping("/")
+public class PdfCreationController {
+  private final PdfCreationFacade pdfCreationFacade;
 
+  public PdfCreationController(PdfCreationFacade pdfCreationFacade) {
+    this.pdfCreationFacade = pdfCreationFacade;
+  }
 
-    @GetMapping("/pdf/user")
-    public ResponseEntity<byte[]> downloadUserPDF()
-    {
+  @GetMapping("/pdf")
+  public ResponseEntity<byte[]> downloadUserPDF() throws Exception {
 
-        final byte[] pdfContent = pdfCreationFacade.createPdfFile();
-        return ResponseEntity.ok()
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=user-info.pdf" )
-            .contentType(MediaType.APPLICATION_PDF)
-            .body(pdfContent);
-    }
+    final byte[] pdfContent = pdfCreationFacade.createPdfFile();
+    return ResponseEntity.ok()
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=user-info.pdf")
+        .contentType(MediaType.APPLICATION_PDF)
+        .body(pdfContent);
+  }
 }
